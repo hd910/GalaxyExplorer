d23408337b024c5b411e65b00056a2001efafa5f
hd910 <hd910@outlook.com>
2017-12-13 16:51:01 +1300

    Get data from Chronozoom and deserialize

---
+++ Assets/Scripts/ChronozoomLoader.cs
@@ -0,0 +1,101 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace GalaxyExplorer
+{
+    public class ChronozoomLoader : MonoBehaviour
+    {
+        public GameObject text;
+
+        private const string ChronozoomURI = "http://www.chronozoom.com/api/gettimelines?supercollection=chronozoom";
+
+        void Awake()
+        {
+            StartCoroutine(GetChronozoomData());
+        }
+
+        IEnumerator GetChronozoomData()
+        {
+            Debug.Log("Getting Chronozoom Data");
+            UnityWebRequest www = UnityWebRequest.Get(ChronozoomURI);
+            yield return www.SendWebRequest();
+
+            if (www.isNetworkError)
+            {
+                Debug.Log(www.error);
+            }
+            else
+            {
+                Debug.Log("Retrieved Chronozoom Data");
+                DeserializeData(www.downloadHandler.text);
+            }
+        }
+
+        void DeserializeData(string data)
+        {
+            
+            Timeline timeline = new Timeline();
+            timeline = JsonConvert.DeserializeObject<Timeline>(data);
+            DisplayData(timeline);
+        }
+
+        void DisplayData(Timeline timeline)
+        {
+            //GameObject textGameObject = Instantiate(text);
+            Debug.Log(timeline);
+        }
+
+    }
+
+    public class Timeline
+    {
+        public string __type { get; set; }
+        public bool FromIsCirca { get; set; }
+        public string Height { get; set; }
+        public string Regime { get; set; }
+        public bool ToIsCirca { get; set; }
+        public float? aspectRatio { get; set; }
+        public string backgroundUrl { get; set; }
+        public string end { get; set; }
+        public Exhibit[] exhibits { get; set; }
+        public string id { get; set; }
+        public string offsetY { get; set; }
+        public string start { get; set; }
+        public Timeline[] timelines { get; set; }
+        public string title { get; set; }
+    }
+
+    public class Exhibit
+    {
+        public string __type { get; set; }
+        public bool IsCirca { get; set; }
+        public string UpdatedBy { get; set; }
+        public string UpdatedTime { get; set; }
+        public List<ContentItem> contentItems { get; set; }
+        public string id { get; set; }
+        public string offsetY { get; set; }
+        public Int64 time { get; set; }
+        public string title { get; set; }
+        public string ParentTimeLineId { get; set; }
+    }
+
+    public class ContentItem
+    {
+        public string __type { get; set; }
+        public Int64 year { get; set; }
+        public string attribution { get; set; }
+        public string description { get; set; }
+        public string id { get; set; }
+        public string mediaSource { get; set; }
+        public string mediaType { get; set; }
+        public int order { get; set; }
+        public string title { get; set; }
+        public string uri { get; set; }
+        public string ParentExhibitId { get; set; }
+        public Int64 ParentExhibitTime { get; set; }
+    }
+}
