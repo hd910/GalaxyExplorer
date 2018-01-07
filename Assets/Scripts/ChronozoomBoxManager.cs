bd77ef6d42b5e46e59b52954583651a6387a9c91
hd910 <haydendo910@hotmail.com>
2018-01-07 14:39:56 +1300

    Register clicks and hovers

---
+++ Assets/Scripts/ChronozoomBoxManager.cs
@@ -0,0 +1,22 @@
+﻿using GalaxyExplorer;
+using HoloToolkit.Unity.InputModule;
+using UnityEngine;
+
+public class ChronozoomBoxManager : GazeSelectionTarget
+{
+    public override void OnGazeSelect()
+    {
+        Debug.Log("Select");
+    }
+
+    public override void OnGazeDeselect()
+    {
+        Debug.Log("Deselect");
+    }
+
+    public override bool OnTapped()
+    {
+        Debug.Log("Tapped");
+        return true;
+    }
+}
