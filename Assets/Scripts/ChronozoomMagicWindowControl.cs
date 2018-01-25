f9126bb160a3b0c24477bc929380eb18890cef7a
hd910 <haydendo910@hotmail.com>
2018-01-26 12:16:58 +1300

    Clicking on magic window enlarges image

---
+++ Assets/Scripts/ChronozoomMagicWindowControl.cs
@@ -0,0 +1,36 @@
+﻿using GalaxyExplorer;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChronozoomMagicWindowControl : GazeSelectionTarget
+{
+
+    public float TravelTime = 1f;
+    public float PresentationDistance = 0.5f;
+
+    private bool isMagnified = false;
+
+    public override bool OnTapped()
+    {
+        Animator animator = gameObject.GetComponent<Animator>();
+
+        if (!isMagnified)
+        {
+            // Start the animation
+            animator.enabled = true;
+            animator.SetBool("Opened", true);
+            isMagnified = true;
+        }
+        else
+        {
+            // Close the animation
+            animator.SetBool("Opened", false);
+            animator.enabled = true;
+            isMagnified = false;
+        }
+        
+
+        return true;
+    }
+}
